fac5932fc8fd48da287dc51b041c7760f1a3b9ad
phongnd <pn463187@gmail.com>
2025-08-06 14:36:07 +0000

    add widgets stats for customer

--- app/Filament/Admin/Widgets/CustomerTableHeader.php
+++ app/Filament/Admin/Widgets/CustomerTableHeader.php
@@ -8,15 +8,38 @@ use Filament\Widgets\Widget;
 class CustomerTableHeader extends Widget
 {
     protected static string $view = 'filament.admin.widgets.customer-table-header';
-
     protected int | string | array $columnSpan = 'full';
-
     public string $search = '';
+    public string $selectedStat = '';
+    public $totalCustomers = 0;
+    public $newCustomers = 0;
+    public $todayBirthdays = 0;
+    public $weekBirthdays = 0;
+    public $monthBirthdays = 0;
+
+    public function mount(): void
+    {
+        $this->totalCustomers = Customer::count();
+        $this->newCustomers = Customer::whereDate('created_at', today())->count();
+        $this->todayBirthdays = Customer::whereMonth('birthday', now()->month)
+            ->whereDay('birthday', now()->day)
+            ->count();
+        $this->weekBirthdays = Customer::whereBetween('birthday', [
+            now()->startOfWeek(),
+            now()->endOfWeek(),
+        ])->count();
+        $this->monthBirthdays = Customer::whereMonth('birthday', now()->month)->count();
+    }
 
     // Gọi mỗi khi giá trị của $search thay đổi (wire:model.live)
     public function updatedSearch(string $value): void
     {
         // Gửi sự kiện 'searchChanged' đến ListCustomers
         $this->dispatch('searchChanged', search: $value);
+    }
+    public function loadCustomer(string $type): void
+    {
+        $this->selectedStat = $type;
+        $this->dispatch('selectedStatChanged', filter: $type);
     }
 }
