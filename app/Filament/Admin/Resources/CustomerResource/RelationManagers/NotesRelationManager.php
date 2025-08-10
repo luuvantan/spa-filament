52e35dae69e57a4db888d14a302b87c9d8396534
phongnd <pn463187@gmail.com>
2025-08-10 18:45:15 +0000

    add history of activities, note and progress table for view customer

---
+++ app/Filament/Admin/Resources/CustomerResource/RelationManagers/NotesRelationManager.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Filament\Admin\Resources\CustomerResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class NotesRelationManager extends RelationManager
+{
+    protected static string $relationship = 'notes';
+    protected static ?string $title = 'Hồ sơ ghi chú';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('description')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->heading('')
+            ->recordTitleAttribute('description')
+            ->columns([
+                Tables\Columns\TextColumn::make('description')
+                    ->label('Nội dung ghi chú'),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->label('Nội dung cập nhật lần cuối'),
+                Tables\Columns\TextColumn::make('customer.name')
+                    ->label('Người tạo')
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
